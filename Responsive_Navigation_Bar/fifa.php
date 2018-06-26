e9c4654fb82982f502c30f7a389a6fd1c7964e7e
Levelsdonchange <rdsowah@st.ug.edu.gh>
2018-06-26 14:13:25 +0000

    Add files via upload

---
+++ Responsive_Navigation_Bar/fifa.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Smart Soccer Quiz</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+
+<div class="container">
+  <center><h2>Welcome to Smart Soccer Quiz</h2></center>
+  <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#home">Quiz</a></li>
+    <li style="float:right";><a data-toggle="tab" href="logout.php">Logout</a></li>
+  </ul>
+    <div class="tab-content">
+        <div id="home" class="tab-pane fade in active"><br>
+        <center><button type="button" class="btn btn-primary">Start Quiz</button></center>
+        </div>
+    </div>
+</div>
+</body>
+</html>
