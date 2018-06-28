8f55183b83acaa57bdf6c84d9e0f61b0962382fa
Levelsdonchange <rdsowah@st.ug.edu.gh>
2018-06-28 15:50:56 +0000

    Add files via upload

---
+++ online-examination-system/project.sql
@@ -0,0 +1,258 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 28, 2018 at 05:42 PM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `email`, `password`) VALUES
+(1, 'sunnygkp10@gmail.com', '123456'),
+(2, 'admin@admin.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answer`
+--
+
+CREATE TABLE `answer` (
+  `qid` text NOT NULL,
+  `ansid` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `answer`
+--
+
+INSERT INTO `answer` (`qid`, `ansid`) VALUES
+('5b34f3bac5bc8', '5b34f3baefb26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` text NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `subject` varchar(500) NOT NULL,
+  `feedback` varchar(500) NOT NULL,
+  `date` date NOT NULL,
+  `time` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `name`, `email`, `subject`, `feedback`, `date`, `time`) VALUES
+('55846be776610', 'testing', 'sunnygkp10@gmail.com', 'testing', 'testing stART', '2015-06-19', '09:22:15pm'),
+('5584ddd0da0ab', 'netcamp', 'sunnygkp10@gmail.com', 'feedback', ';mLBLB', '2015-06-20', '05:28:16am'),
+('558510a8a1234', 'sunnygkp10', 'sunnygkp10@gmail.com', 'dl;dsnklfn', 'fmdsfld fdj', '2015-06-20', '09:05:12am'),
+('5585509097ae2', 'sunny', 'sunnygkp10@gmail.com', 'kcsncsk', 'l.mdsavn', '2015-06-20', '01:37:52pm'),
+('5586ee27af2c9', 'vikas', 'vikas@gmail.com', 'trial feedback', 'triaal feedbak', '2015-06-21', '07:02:31pm'),
+('5589858b6c43b', 'nik', 'nik1@gmail.com', 'good', 'good site', '2015-06-23', '06:12:59pm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `email` varchar(50) NOT NULL,
+  `eid` text NOT NULL,
+  `score` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `history`
+--
+
+INSERT INTO `history` (`email`, `eid`, `score`, `level`, `sahi`, `wrong`, `date`) VALUES
+('admin@admin.com', '5b34f36536e54', 1, 1, 1, 0, '2018-06-28 14:46:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `options`
+--
+
+CREATE TABLE `options` (
+  `qid` varchar(50) NOT NULL,
+  `option` varchar(5000) NOT NULL,
+  `optionid` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `options`
+--
+
+INSERT INTO `options` (`qid`, `option`, `optionid`) VALUES
+('5b34f3bac5bc8', ' 3', '5b34f3baefb1c'),
+('5b34f3bac5bc8', ' 4', '5b34f3baefb26'),
+('5b34f3bac5bc8', ' 5', '5b34f3baefb27'),
+('5b34f3bac5bc8', ' 6', '5b34f3baefb28'),
+('5b34f3bac5bc8', ' 7', '5b34f3baefb2a');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `eid` text NOT NULL,
+  `qid` text NOT NULL,
+  `qns` text NOT NULL,
+  `choice` int(10) NOT NULL,
+  `sn` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`eid`, `qid`, `qns`, `choice`, `sn`) VALUES
+('5b34f36536e54', '5b34f3bac5bc8', 'How many venues were used for the 2010 African cup of Nations?\r\n', 5, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `eid` text NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `time` bigint(20) NOT NULL,
+  `intro` text NOT NULL,
+  `tag` varchar(100) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`eid`, `title`, `sahi`, `wrong`, `total`, `time`, `intro`, `tag`, `date`) VALUES
+('5b34f36536e54', 'African Cup Of Nations', 1, 1, 1, 15, '', '', '2018-06-28 14:40:37');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rank`
+--
+
+CREATE TABLE `rank` (
+  `email` varchar(50) NOT NULL,
+  `score` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `rank`
+--
+
+INSERT INTO `rank` (`email`, `score`, `time`) VALUES
+('sunnygkp10@gmail.com', 9, '2015-06-24 03:22:38'),
+('avantika420@gmail.com', 8, '2015-06-23 14:49:39'),
+('mi5@hollywood.com', 4, '2015-06-23 15:12:56'),
+('nik1@gmail.com', 1, '2015-06-23 16:11:50'),
+('rdsowah@st.ug.edu.gh', 0, '2018-06-28 15:33:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `name` varchar(50) NOT NULL,
+  `gender` varchar(5) NOT NULL,
+  `college` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mob` bigint(20) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`name`, `gender`, `college`, `email`, `mob`, `password`) VALUES
+('Mark Zukarburg', 'M', 'Stanford', 'ceo@facebook.com', 987654321, 'e10adc3949ba59abbe56e057f20f883e'),
+('Richmond Sowah', 'M', '', 'rdsowah@st.ug.edu.gh', 540819824, '7e0d7f8a5d96c24ffcc840f31bce72b2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
